c04b573e5ca07e97e78461f5a9a898d4adddcf9c
shi-n <shi-n_xyz@cloudsquare.jp>
2020-05-08 20:53:00 +0900

    Initial Commit

---
+++ ExampleWatchComplication WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  ExampleWatchComplication WatchKit Extension
+//
+//  Created by Shin Inaba on 2020/05/08.
+//  Copyright © 2020 shi-n. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ ExampleWatchComplication WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  ExampleWatchComplication WatchKit Extension
+//
+//  Created by Shin Inaba on 2020/05/08.
+//  Copyright © 2020 shi-n. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
